c8d4beeae50b53ea0fcf18ac2117d479649b9bfa
Vinicius Oliveira <vinicius.deoliveira@aluno.ufabc.edu.br>
2020-12-02 17:51:03 -0300

    Finalizado exercicios sobre componentes

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Saudacao from "./componentes/Saudacao";
+import Pai from "./componentes/Pai";
+import Filho from "./componentes/Filho"
 
 ReactDOM.render(
   <div>
-    <Saudacao tipo="Bom dia" nome="Belchior" />
+    <Pai nome="Antonio" sobrenome="Belchior">
+        <Filho nome="Pedro" />
+        <Filho nome="Carlos" />
+        <Filho nome="Eduarda" />
+    </Pai>
   </div>,
   document.getElementById("root")
 );
