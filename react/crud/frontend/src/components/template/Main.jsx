d3fff33b4d1b2653b1f8a1d1992dcdeb595a8892
Vinicius Oliveira <vinicius.deoliveira@aluno.ufabc.edu.br>
2020-12-03 14:49:45 -0300

    Implementacao componente header

--- react/crud/frontend/src/components/template/Main.jsx
+++ react/crud/frontend/src/components/template/Main.jsx
@@ -1,11 +1,10 @@
-import './Main.css'
-import React from 'react'
-import Header from './Header'
+import "./Main.css";
+import React from "react";
+import Header from "./Header";
 
-export default props => 
-    <React.Fragment>
-        <Header />
-        <main className="content">
-            Conteúdo
-        </main>
-    </React.Fragment>
+export default (props) => (
+  <React.Fragment>
+    <Header {...props} />
+    <main className="content">Conteúdo</main>
+  </React.Fragment>
+);
