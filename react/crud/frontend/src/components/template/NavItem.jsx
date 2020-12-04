61d99aa90c258be865305e566e6c726ae63a2274
Vinicius Oliveira <vinicius.deoliveira@aluno.ufabc.edu.br>
2020-12-04 11:13:39 -0300

    Implementado sistema de Routes do React

--- react/crud/frontend/src/components/template/NavItem.jsx
+++ react/crud/frontend/src/components/template/NavItem.jsx
@@ -1,8 +1,9 @@
 import "./NavItem.css";
 import React from "react";
+import { Link } from 'react-router-dom'
 
 export default (props) => (
-  <a href={`#/${props.url}`}>
+  <Link to={`/${props.url}`}>
     <i className={`fa fa-${props.icon}`}></i> {props.label}
-  </a>
+  </Link>
 );
--- react/crud/frontend/src/components/user/UserCrud.jsx
+++ react/crud/frontend/src/components/user/UserCrud.jsx
@@ -1,4 +1,4 @@
-import React, { component } from 'react'
+import React, { Component } from 'react'
 import Main from '../template/Main'
 
 const headerProps = {
