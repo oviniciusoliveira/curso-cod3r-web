f1a96d10524bf3820a565f7baf87dfcfbfa9be9c
Vinicius Oliveira <vinicius.deoliveira@aluno.ufabc.edu.br>
2020-12-08 17:39:05 -0300

    Criacao componentes

---
+++ vue/calculator/src/main/Calculator.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="calculator">
+        <Display />
+        <Button />
+    </div>
+</template>
+
+<script>
+import Display from "../components/Display";
+import Button from "../components/Button";
+export default {
+    components: { Button, Display }
+};
+</script>
+
+<style>
+    .calculator {
+        height: 320px;
+        width: 235px;
+        border-radius: 5px;
+        overflow: hidden;
+
+        display: grid;
+        grid-template-columns: repeat(4, 25px);
+        grid-template-rows: 1fr repeat(5, 48px);
+    }
+</style>
