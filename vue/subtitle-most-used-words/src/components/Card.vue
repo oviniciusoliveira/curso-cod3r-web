a963333ae3425edad1eb6f0dffebf409579a16a8
Vinicius Oliveira <vinicius.deoliveira@aluno.ufabc.edu.br>
2020-12-11 17:11:07 -0300

    Iniciado projeto Most Words in Subtitles

---
+++ vue/subtitle-most-used-words/src/components/Card.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="card">
+    <span class="name">{{ name }}</span>
+    <span class="amount">{{ amount }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name", "amount"],
+};
+</script>
+
+// scoped = esse estilo só funciona para este componente
+<style scoped>
+    .card {
+        margin: 10px;
+        height: 55px;
+        border-radius: 30px;
+        background-color: #dd1133;
+        color: #eeeeee;
+        font-size: 1.3rem;
+        font-weight: bold;
+
+        display: flex;
+        align-items: center;
+    }
+
+    .card .name {
+        margin-left: 15px;
+    }
+    
+    .card .amount {
+        margin: 10px;
+        padding: 5px;
+        height: 35px;
+        width: 35px;
+        border-radius: 18px;
+
+        background-color: #eeeeee;
+        color: #dd1133;
+
+        font-size: 14px;
+
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
---
+++ vue/subtitle-most-used-words/src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container fluid>
+    <div class="cards">
+      <Card
+        v-for="word in groupedWords"
+        :key="word.name"
+        :name="word.name"
+        :amount="word.amount"
+      />
+    </div>
+  </v-container>
+</template>
+
+<script>
+import Card from "./Card";
+export default {
+  components: { Card },
+  data: function() {
+    return {
+      groupedWords: [
+        { name: "i", amount: 1243 },
+        { name: "you", amount: 900 },
+        { name: "he", amount: 853 },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+  .cards {
+    display: flex;
+    flex-wrap: wrap;
+  }
+</style>
