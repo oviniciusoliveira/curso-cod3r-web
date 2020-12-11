922ec2cbf7f37fa2ddac382ab251a036781e66fe
Vinicius Oliveira <vinicius.deoliveira@aluno.ufabc.edu.br>
2020-12-11 19:20:17 -0300

    Iniciando processamento dos arquivos

--- vue/subtitle-most-used-words/src/backend/index.js
+++ vue/subtitle-most-used-words/src/backend/index.js
@@ -1,10 +1,19 @@
 const { ipcMain } = require("electron");
+
+const pathsToRows = require("./pathsToRows");
+
 // Inter process communication
 ipcMain.on("process-subtitles", (event, paths) => {
   console.log(paths);
 
-  event.reply("process-subtitles", [
-    { name: "i", amount: 1234 },
-    { name: "you", amount: 334 },
-  ]);
+  pathsToRows(paths)
+    .then((rows) => console.log(rows))
+    .then(() => {
+      event.reply("process-subtitles", [
+        {
+          name: "i",
+          amount: 1234,
+        },
+      ]);
+    });
 });
