db056a85fc3c843f5a4e98eaa65ac3b8717d22f9
Vinicius Oliveira <vinicius.deoliveira@aluno.ufabc.edu.br>
2020-12-16 18:31:07 -0300

    Implementacao de diretivas estruturais como exercicio de aprendizagem

--- angular/projeto-crud/frontend/src/app/app.module.ts
+++ angular/projeto-crud/frontend/src/app/app.module.ts
@@ -16,9 +16,10 @@ import { HomeComponent } from './views/home/home.component';
 import { MatCardModule } from "@angular/material/card";
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent, ProductCrudComponent, RedDirective],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent, ProductCrudComponent, RedDirective, ForDirective],
   imports: [
     BrowserModule,
     AppRoutingModule,
