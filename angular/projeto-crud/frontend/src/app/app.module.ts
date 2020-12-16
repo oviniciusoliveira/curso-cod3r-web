fd7f051005e80cd1e7254108d4c730aa4ead4f5d
Vinicius Oliveira <vinicius.deoliveira@aluno.ufabc.edu.br>
2020-12-16 18:35:10 -0300

    Criacao do componente product-create e sua respectiva rota na aplicacao

--- angular/projeto-crud/frontend/src/app/app.module.ts
+++ angular/projeto-crud/frontend/src/app/app.module.ts
@@ -17,9 +17,10 @@ import { MatCardModule } from "@angular/material/card";
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
 import { ForDirective } from './directives/for.directive';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent, ProductCrudComponent, RedDirective, ForDirective],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent, ProductCrudComponent, RedDirective, ForDirective, ProductCreateComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
